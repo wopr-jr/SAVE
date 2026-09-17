06fcd4e60728178b28f43e721dae8abbe6c5c316
wopr-jr <joinwill123@gmail.com>
2026-09-17 15:04:35 -0400

    bug fix

--- cli/cli.py
+++ cli/cli.py
@@ -5,8 +5,8 @@ from dataclasses import dataclass
 from pathlib import Path
 from typing import Protocol
 
-from SAVE.cli.commands.convert import command_convert_file
-from SAVE.cli.commands.import import command_import_file
+from SAVE.cli.commands.convert_file import command_convert_file
+from SAVE.cli.commands.import_file import command_import_file
 from SAVE.cli.commands.inspect import command_inspect_stored_checklist
 
 from SAVE.cli.cli_common import ExitCode
